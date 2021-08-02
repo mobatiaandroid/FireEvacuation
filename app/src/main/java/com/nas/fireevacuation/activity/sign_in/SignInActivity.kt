12eb62093011823f8274e768c2a26f99cf5c562d
SanjuSabu2 <sabusanju2@gmail.com>
2021-08-02 18:28:18 +0530

    Initial Commit

---
+++ app/src/main/java/com/nas/fireevacuation/activity/sign_in/SignInActivity.kt
@@ -0,0 +1,12 @@
+package com.nas.fireevacuation.activity.sign_in
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nas.fireevacuation.R
+
+class SignInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+}
---
+++ app/src/main/java/com/nas/fireevacuation/activity/welcome/WelcomeActivity.kt
@@ -0,0 +1,12 @@
+package com.nas.fireevacuation.activity.welcome
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.nas.fireevacuation.R
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_welcome)
+    }
+}
